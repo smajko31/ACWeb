be3bf2f28140b2e80d4130854e0eaa9754023e05
smajko31 <smajko31@outlook.com>
2018-09-11 21:16:46 -0700

    add javascript for sticky navbar

---
+++ ACWeb/ACWeb/Scripts/Layout/navbar.js
@@ -0,0 +1,12 @@
+﻿$(document).scroll(function () {
+    var documentScrollTop = $(document).scrollTop();
+    if (documentScrollTop > $('#header').height()) {
+        $('#navbar').addClass('fixedattop');
+        var shiftContent = $('#navbar').height();
+        $('#content').css('margin-top', shiftContent + 'px');
+    }
+    else if ($('#navbar').hasClass('fixedattop')) {
+        $('#navbar').removeClass('fixedattop');
+        $('#content').css('margin-top', '0px');
+    }
+});
