0c30eb94fbbc2aa8d6dc3a94c3e13a9e7e09dd11
smajko31 <smajko31@outlook.com>
2018-09-12 21:51:56 -0700

    fix routing

--- ACWeb/ACWeb/Controllers/MainController.cs
+++ ACWeb/ACWeb/Controllers/MainController.cs
@@ -13,10 +13,8 @@ namespace ACWeb.Controllers
             return View();
         }
 
-        public ActionResult About()
+        public ActionResult Services()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
