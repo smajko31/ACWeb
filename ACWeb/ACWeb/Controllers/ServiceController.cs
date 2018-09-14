923a2c05737298effe27863782a6e54ea8283c61
Sebastian Majko <smajko31@gmail.com>
2018-09-14 16:17:56 -0700

    implement contact form post

---
+++ ACWeb/ACWeb/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ACWeb.Models;
+
+namespace ACWeb.Controllers
+{
+    public class ServiceController : Controller
+    {
+        [HttpPost]
+        public ActionResult Contact(ContactInfo ci)
+        {
+            return View();
+        }
+    }
+}
