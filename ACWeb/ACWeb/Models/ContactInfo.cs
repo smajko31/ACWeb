231b6eb08380a97316cff79f9db5f02ec58238e8
smajko31 <smajko31@gmail.com>
2018-09-15 12:39:50 -0700

    add email system

--- ACWeb/ACWeb/Models/ContactInfo.cs
+++ ACWeb/ACWeb/Models/ContactInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 
 namespace ACWeb.Models
@@ -12,5 +13,17 @@ namespace ACWeb.Models
         public string Phone { get; set; }
         public string Subject { get; set; }
         public string Message { get; set; }
+
+        public void Send()
+        {
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("your email address");
+            message.To.Add(new MailAddress("your recipient"));
+            message.Subject = this.Subject;
+            message.Body = String.Format("Name: {0}\nEmail: {1}\nPhone: {2}\n\n Message{3}: ", this.Name, this.Email, this.Phone, this.Message);
+
+            SmtpClient client = new SmtpClient();
+            client.Send(message);
+        }
     }
 }
